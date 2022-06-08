623ce817c5274810d11046bf13dee9a57e7afb2f
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-06-07 22:43:13 -0400

    working on admin

--- skyrimdb/skyrim_app/admin.py
+++ skyrimdb/skyrim_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CharacterAd(admin.ModelAdmin):
+    list_display = ('name', 'life_points', 'breed', 'weakness',)
+
+class TypeOfDamageAd(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Character, CharacterAd)
+admin.site.register(TypeOfDamage, TypeOfDamageAd)

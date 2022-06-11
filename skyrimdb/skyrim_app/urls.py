5ebe050497a5c1b1a0ae3074a906dae672ec6cd9
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-06-10 23:44:28 -0400

    working on templates

--- skyrimdb/skyrim_app/urls.py
+++ skyrimdb/skyrim_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('tables/players', views.players, name='players'),
+    path('tables/battles', views.battles, name='battles'),
+    path('tables/beasts', views.beasts, name='beasts'),
+    path('tables/spells', views.spells, name='spells'),
 ]
--- skyrimdb/skyrim_app/views.py
+++ skyrimdb/skyrim_app/views.py
@@ -8,3 +8,15 @@ def about(request):
 
 def contact(request):
     return render(request, 'pages/contact.html')
+
+def players(request):
+    return render(request, 'pages/tables_pages/players.html')
+
+def battles(request):
+    return render(request, 'pages/tables_pages/battles.html')
+
+def beasts(request):
+    return render(request, 'pages/tables_pages/beasts.html')
+
+def spells(request):
+    return render(request, 'pages/tables_pages/spells.html')

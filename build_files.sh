669c5c73c3d8c91af907d8c6ae403f069fafef56
Dennis Fiallo <dfiallo35@gmail.com>
2024-08-04 16:27:01 -0400

    update deploy vercel

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python skyrimdb/manage.py makemigrations --noinput
-python skyrimdb/manage.py migrate --noinput
+python3.9 skyrimdb/manage.py makemigrations --noinput
+python3.9 skyrimdb/manage.py migrate --noinput
 
 echo "Collect Static..."
-python skyrimdb/manage.py collectstatic --noinput --clear
+python3.9 skyrimdb/manage.py collectstatic --noinput --clear
